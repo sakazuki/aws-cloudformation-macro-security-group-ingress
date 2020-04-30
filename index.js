8c781af2a5f54d98a54e62bdc80cfa87808079e0
sakazuki <sakazuki@gmail.com>
2020-05-01 08:39:55 +0900

    Add AllowCidr parameter

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ exports.handler = async (event) => {
   console.log(event)
   if (event.params.AllowPorts) {
     const ingress = []
+    const cidrIp = event.params.AllowCidr || '0.0.0.0/0'
     for (const item of event.params.AllowPorts) {
       const [fromPort, toPort = fromPort] = item.split('-')
       ingress.push({
         IpProtocol: 'tcp',
         FromPort: fromPort,
         ToPort: toPort,
-        CidrIp: '0.0.0.0/0'
+        CidrIp: cidrIp
       })
     }
     event.fragment = ingress
